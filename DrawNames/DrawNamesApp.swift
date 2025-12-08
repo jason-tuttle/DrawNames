00a9c5cb0b6a766ff6e25c0a2711b5e9e323797e
Jason Tuttle <jason@hearspotbark.com>
2025-12-08 14:58:04 -0500

    Initial Commit

---
+++ DrawNames/DrawNamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawNamesApp.swift
+//  DrawNames
+//
+//  Created by Jason Tuttle on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct DrawNamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrawNamesTests/DrawNamesTests.swift
@@ -0,0 +1,17 @@
+//
+//  DrawNamesTests.swift
+//  DrawNamesTests
+//
+//  Created by Jason Tuttle on 12/8/25.
+//
+
+import Testing
+@testable import DrawNames
+
+struct DrawNamesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
